4c794c746fd394bb0eddba9d6b1bf59af88955e6
ted <ted@ted-Lenovo-IdeaPad-Y550P.(none)>
2012-04-05 00:13:30 -0700

    1st

--- server.js
+++ server.js
@@ -1,5 +1,47 @@
 var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\nApp (dasquetdania) is running on Node.JS ' + process.version);
-}).listen(process.env['app_port'] || 3000);
+var fs = require('fs');
+
+
+function onRequest(req, res) { 
+  var pathname = url.parse(req.url).pathname;
+  console.log("new connection");
+  if (pathname = '/home') {
+    
+    fs.readFile(__dirname + '/files/index.html', function(err, data){
+      res.writeHead(200, {'Content-Type':'text/html'});
+      res.write(data);
+      res.end();
+  
+    });
+  }
+
+  if (pathname = '/createaccount') {
+    fs.readFile(__dirname + '/files/createaccount.html', function(err, data) { 
+      res.writeHead(200, {'Content-Type':'text/html'});
+      res.write(data);
+      res.end();
+
+    });
+  }
+  if (pathname = '/user' /*regexStringsNumbers*/) {
+    fs.readFile(__dirname + '/files/user1.html', function(err, data) { 
+      res.writeHead(200, {'Content-Type':'text/html'});
+      res.write(data);
+      res.end();
+    });
+}
+
+
+server = http.createServer(onRequest); 
+//  fs.readFile(__dirname + '/files/index.html', function(err, data){
+//    res.writeHead(200, {'Content-Type':'text/html'});
+//    res.write(data);
+//    res.end();
+//
+//
+//  });
+
+
+server.listen(process.env['app_port'] || 3000);
+
+console.log('server started');
