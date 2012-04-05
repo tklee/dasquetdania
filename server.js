2e262d478be6851224f8706947da7cd616e7c533
ted <ted@ted-Lenovo-IdeaPad-Y550P.(none)>
2012-04-05 00:20:35 -0700

    none

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 
 function onRequest(req, res) { 
   var pathname = url.parse(req.url).pathname;
-  console.log("new connection");
+  //console.log("new connection");
   if (pathname = '/home') {
     
     fs.readFile(__dirname + '/files/index.html', function(err, data){
@@ -15,20 +15,20 @@ function onRequest(req, res) {
     });
   }
 
-  if (pathname = '/createaccount') {
-    fs.readFile(__dirname + '/files/createaccount.html', function(err, data) { 
-      res.writeHead(200, {'Content-Type':'text/html'});
-      res.write(data);
-      res.end();
-
-    });
-  }
-  if (pathname = '/user' /*regexStringsNumbers*/) {
-    fs.readFile(__dirname + '/files/user1.html', function(err, data) { 
-      res.writeHead(200, {'Content-Type':'text/html'});
-      res.write(data);
-      res.end();
-    });
+//  if (pathname = '/createaccount') {
+//    fs.readFile(__dirname + '/files/createaccount.html', function(err, data) { 
+//      res.writeHead(200, {'Content-Type':'text/html'});
+//      res.write(data);
+//      res.end();
+//
+//    });
+//  }
+//  if (pathname = '/user' /*regexStringsNumbers*/) {
+//    fs.readFile(__dirname + '/files/user1.html', function(err, data) { 
+//      res.writeHead(200, {'Content-Type':'text/html'});
+//      res.write(data);
+//      res.end();
+//    });
 }
 
 
